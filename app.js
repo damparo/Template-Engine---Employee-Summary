723b1d0458ae8f77011bac77e09b288b4382d937
Dennis Amparo <dennisamparo11@gmail.com>
2020-03-04 18:33:57 -0700

    needsprefinalreview

--- app.js
+++ app.js
@@ -12,92 +12,222 @@ const render = require("./classes/htmlRenderer");
 // console.log(new Intern("name", 1, "tucker@gmail.com", "tuckerschol"));
 
 // Write code to use inquirer to gather information about the development team members,
+const overallTeam = [];
+
+function managerQs () {
+    inquirer
+        .prompt ([
+            {
+                type: "input",
+                name: "name",
+                message: "what is your manager's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "what is your manager's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "what is your manager's email?"
+            },
+            {
+                type: "input",
+                name: "officenumber",
+                message: "what is your manager's office number?"
+            },
+        ]).then(function(answers){
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officenumber);
+            overallTeam.push(manager);
+            teamBuilder()
+        })
+}
+function engineerQs () {
+    inquirer
+        .prompt ([
+            {
+                type: "input",
+                name: "name",
+                message: "what is your engineer's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "what is your engineer's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "what is your engineer's email?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "what is your engineer's github?"
+            },
+        ]).then(function(answers){
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            overallTeam.push(engineer);
+            teamBuilder()
+        })
+}
+function internQs () {
+    inquirer
+        .prompt ([
+            {
+                type: "input",
+                name: "name",
+                message: "what is your intern's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "what is your intern's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "what is your intern's email?"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "what is your intern's school?"
+            },
+        ]).then(function(answers){
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            overallTeam.push(intern);
+            teamBuilder()
+        })
+}
+
+
+
+function done () {
+    console.log ("You're done!");
+    fs.writeFileSync(outputPath, render(overallTeam), "utf-8");
+}
+// use write file sync so answers are replaced instead of creating a new file
+
+
+
 
 // Manager
+function teamBuilder ()
+     {
 inquirer
-    .prompt([
-    {
-        name: "name",
-        message: "Please build your team"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your manager's name?"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your manager's id?"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your manager's email?"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your manager's office number?"
-    },
+    .prompt(
     {
         type: "list",
-        name: "name",
+        name: "TeamMember",
         message: "which type of team member would you like to add?",
-        choices: ["Engineer", "Intern"]
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your Engineer's name?"
-    
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your Engineer's id?"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your Engineer's email?"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your Engineer's Github username?"
-    },
-    {
-        type: "list",
-        name: "name",
-        message: "which type of team member would you like to add?",
-        choices: ["Engineer", "Intern"]
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your Intern's name?"
-    
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your Intern's id?"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your Intern's email?"
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "what is your Intern's school?"
-    },
+        choices: ["Manager", "Engineer", "Intern", "Done" ]
+    }
+    ).then(function(answers){
+        switch (answers.TeamMember) {
+            
+    case "Manager":
+    managerQs();
+    break;
+    case "Engineer":
+    engineerQs();
+    break;
+    case "Intern":
+    internQs();
+    break;
+    case "Done":
+    done();
+    break;
+        }
+    })
 
 
-])
-.then(answers => { console.info('answers', answers)});
+
+}
+
+teamBuilder()
+
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your manager's name?"
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your manager's id?"
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your manager's email?"
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your manager's office number?"
+//     },
+
+
+//     {
+//         type: "list",
+//         name: "name",
+//         message: "which type of team member would you like to add?",
+//         choices: ["Manager", "Engineer", "Intern", "Done" ]
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your Engineer's name?"
+    
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your Engineer's id?"
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your Engineer's email?"
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your Engineer's Github username?"
+//     },
+//     {
+//         type: "list",
+//         name: "name",
+//         message: "which type of team member would you like to add?",
+//         choices: ["Engineer", "Intern"]
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your Intern's name?"
+    
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your Intern's id?"
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your Intern's email?"
+//     },
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "what is your Intern's school?"
+//     },
+
+
+// ])
+// .then(answers => { console.info('answers', answers)});
 
 // and to create objects for each team member (using the correct classes as blueprints!)
 // After the user has input all employees desired, call the `render` function (required
